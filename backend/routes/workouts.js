d45890d1885167427ef9690477ac2610c5afb564
dogogo-wu <dog9zero0@gmail.com>
2022-07-09 11:33:08 +0800

    React basic setup

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,34 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require('../models/workoutModel')
+const {
+    getWorkOuts,
+    getWorkOut,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
-router.get('/', (req, res) => {
-    res.json({ mssg: 'Show all workouts.' });
-})
+router.get('/', getWorkOuts)
 
-router.post('/', async(req, res) => {
+router.get('/:id', getWorkOut)
 
-    const {title, load, reps} = req.body;
-    try {
-        const workout = await Workout.create({
-            title, load, reps
-        });
-        res.status(200).json(workout);
-    } catch (err) {
-        res.status(400).json({ error: err.message })
-    }
-})
+router.post('/', createWorkout)
 
-router.get('/:id', (req, res) => {
-    res.json({ mssg: 'Show one workout.' });
-})
+router.delete('/:id', deleteWorkout)
 
-router.delete('/:id', (req, res) => {
-    res.json({ mssg: 'Delete a workout.' });
-})
+router.patch('/:id', updateWorkout)
 
-router.patch('/:id', (req, res) => {
-    res.json({ mssg: 'Update a workout.' });
-})
 
 module.exports = router
